183312b1c598c7e680010c1902a0fe918d604ad3
nrb2002 <tshibasubaron@yahoo.fr>
2026-05-13 22:24:10 +0100

    Fixed Get all endpoint
    Fixed all bugs for this endpoint.

--- src/config/db.js
+++ src/config/db.js
@@ -2,23 +2,34 @@ const { MongoClient } = require("mongodb");
 
 const client = new MongoClient(process.env.MONGO_URI); //MongoDB Atlas connection string saved in .env file, format: mongodb+srv://<username>:<password>@cluster0.mongodb.net/?retryWrites=true&w=majority
 
-const dbName = process.env.DB_NAME; //Database name saved in .env file, default to "contactsDB" if not provided
+const dbName = process.env.DB_NAME || "contactsDB"; //Database name saved in .env file, default to "contactsDB" if not provided
 
-let collection;
+let database;
 
 const connectDB = async () => {
-  await client.connect();
-  
-  collection = client.db(dbName);
+  await client.connect();  
+  database = client.db(dbName);
   console.log("Connected to MongoDB Atlas"); //For testing
 }
 
+const getDB = () => {
+  if (!database) {
+    throw new Error("Database not connected. Call connectDB first.");
+  }
+  return database;
+};
+
 //Get the collection to perform CRUD operations on
-function getCollection() {
-  return collection;
+function getCollection(name) {
+  if (!database) {
+    throw new Error("Database not connected. Call connectDB first.");
+  }
+
+  return database.collection(name);
 }
 
 module.exports = {
   connectDB,
-  getCollection
+  getCollection,
+  getDB
 };
