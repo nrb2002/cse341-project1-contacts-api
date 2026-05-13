ecb6b660ef7b12ae97bd87ea95d04d13ceaf864f
nrb2002 <tshibasubaron@yahoo.fr>
2026-05-13 22:24:15 +0100

    Update contacts.service.js

--- src/services/contacts.service.js
+++ src/services/contacts.service.js
@@ -1,42 +1,45 @@
-//Import dependencies
 const { ObjectId } = require("mongodb");
 const { getCollection } = require("../config/db");
 
-//Get the collection to perform CRUD operations on
-const collection = getCollection();
-
-/** Prepare all endpoints services */
-
-//Get all contacts
+// Get all contacts
 const getAllContacts = async () => {
-  return await collection.find({}).toArray();
+  return await getCollection("contacts")
+    .find({})
+    .toArray();
 };
 
-//Get single contact by id
+// Get single contact
 const getContactById = async (id) => {
-  return await collection.findOne({ _id: new ObjectId(id) });
+  return await getCollection("contacts")
+    .findOne({ _id: new ObjectId(id) });
 };
 
-//Create new contact
+// Create contact
 const createContact = async (contactData) => {
-  const result = await collection.insertOne(contactData);
+  const result = await getCollection("contacts")
+    .insertOne(contactData);
+
   return result.insertedId;
 };
 
-//Update contact by id
+// Update contact
 const updateContact = async (id, updatedData) => {
-  delete updatedData._id; //prevent _id overwrite
-  
-  const result = await collection.updateOne(
-    { _id: new ObjectId(id) },
-    { $set: updatedData }
-  );
+  delete updatedData._id;
+
+  const result = await getCollection("contacts")
+    .updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updatedData }
+    );
+
   return result.modifiedCount > 0;
 };
 
-//Delete contact by id
+// Delete contact
 const deleteContact = async (id) => {
-  const result = await collection.deleteOne({ _id: new ObjectId(id) });
+  const result = await getCollection("contacts")
+    .deleteOne({ _id: new ObjectId(id) });
+
   return result.deletedCount > 0;
 };
 
