29ec9b47aa44b42856564bc619aba3109c396edf
nrb2002 <tshibasubaron@yahoo.fr>
2026-05-16 20:32:34 +0100

    Update swagger.js

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -7,6 +7,8 @@ const doc = {
         title: 'Contacts API Documentation',
         version: "1.0.0",
         description: 'This is an API for storing and retrieving information about contacts. These contacts are to be stored in a MongoDB database and all interaction happen through the API. The API can be used by any frontend.',
+        author:'Baron T.',
+        'Last update': '2024-06-01'
     },
 
     /**
@@ -14,7 +16,7 @@ const doc = {
      */
 
     //For production
-    host: process.env.NODE_ENV === 'production' ? 'https://cse341-project1-contacts-api.onrender.com/api-docs/' : 'localhost:8080',
+    host: process.env.NODE_ENV === 'production' ? 'cse341-project1-contacts-api.onrender.com/' : 'localhost:8080',
     schemes: process.env.NODE_ENV === 'production' ? ['https'] : ['http']
 
     //For local testing
