014fe15f5d40ed3a4bb6b4315b2d983c5909c9c6
nrb2002 <tshibasubaron@yahoo.fr>
2026-05-17 07:20:50 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+// const swaggerRoutes = require("./swagger.routes");
 
-//router.use('/', require('./swaggerRoutes'));
+// router.use('/', require('./swaggerRoutes'));
 
 router.get("/", (req, res) => {
   //#swagger.tags=['Home Page']
